0ba2eacaf47ab1620ee371775da85c9f02307e9c
Matthew Courtney <matthewdc182@gmail.com>
2020-08-15 00:39:55 -0400

    tried to figure out the search method but its not working cant get it to return any city except charlotte the variable wont pick up the text i put in the search key so i jsut put charlotte in thte URL

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,19 @@
 var apiKey = "23346a4286e014dc1c7ba261140705ca"; 
-var prevSearch = JSON.parse(localStorage.getItem("weather")); //calls item from local storage mac and ryan helped me with this i still cant get it to work 
-var city = $("#cityName").val();
+var city = $("#city").val();
+var prevSearch = JSON.parse(localStorage.getItem("city")) || []; //calls item from local storage mac and ryan helped me with this i still cant get it to work 
+
 
 //stores search item to local storage
 $("#searchBtn").on("click", function () {
     prevSearch.push(city)
-    localStorage.setItem("weather", JSON.stringify(prevSearch))
+    localStorage.setItem("city", JSON.stringify(prevSearch))
     showWeather(city)
 })
 
 /// makes the main jumbotron with info adds 
 function showWeather() {
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=" + apiKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Charlotte&units=imperial&appid=" + apiKey;
 
     $.ajax({
         url: queryURL,
@@ -36,18 +37,18 @@ function showWeather() {
 }
 
 
-$.ajax("https://api.openweathermap.org/data/2.5/forecast?q=London&appid=23346a4286e014dc1c7ba261140705ca")
+$.ajax("https://api.openweathermap.org/data/2.5/forecast?q=Charlotte&appid=23346a4286e014dc1c7ba261140705ca")
 .then(function (response) {
     console.log(response);
 
-    var array = response.list
+    var array = response.list;
 
     for (let i = 0; i < array.length; i+=8) {  //made a for loop that loops over the hourly array increased it by 8 to get a differnt day each time
-        var card = $("<div>").addClass("card bg-danger col-md-2") // added the cards
-        var date = $("<div>").text(array[i].dt_txt) // added the date
-        var temp = $("<div>").text(array[i].main.temp + "F")  // temp
-        var humidity = $("<div>").text( array[i].main.humidity) ./// humidity
-        card.append(date, temp, humidity) //apended the info into the cards
-        $("#futureWeather").append(card) //appended the cards to the future weather div 
+        var card = $("<div>").addClass("card bg-danger col-md-2"); // added the cards
+        var date = $("<div>").text(array[i].dt_txt); // added the date
+        var temp = $("<div>").text(array[i].main.temp + "F");  // temp
+        var humidity = $("<div>").text( array[i].main.humidity); /// humidity
+        card.append(date, temp, humidity); //apended the info into the cards
+        $("#futureWeather").append(card); //appended the cards to the future weather div 
     }
 })
